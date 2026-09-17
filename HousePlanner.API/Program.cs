779040437129ee9b9fce4edf8baf06759bb0bf0e
IT24101458-M.G.M. Sanvidu <165678028+ManithSanvidu@users.noreply.github.com>
2026-09-17 22:01:33 +0530

    feat: fixed error occurring in backend

--- HousePlanner.API/Program.cs
+++ HousePlanner.API/Program.cs
@@ -65,6 +65,9 @@ builder.Services.AddSwaggerGen(c =>
 
 // 4. Register application services
 builder.Services.AddScoped<IFirebaseAuthService, FirebaseAuthService>();
+builder.Services.AddScoped<ICurrentUserContextService, CurrentUserContextService>();
+builder.Services.AddScoped<IPreDesignedPlanLayoutValidator, PreDesignedPlanLayoutValidator>();
+builder.Services.AddScoped<PreDesignedPlanSeeder>();
 builder.Services.AddHttpClient("AgenticService", client =>
 {
     client.BaseAddress = new Uri(builder.Configuration["AgenticService:BaseUrl"] ?? "http://localhost:8001");
@@ -137,6 +140,7 @@ using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     context.Database.Migrate();
+    await scope.ServiceProvider.GetRequiredService<PreDesignedPlanSeeder>().SeedAsync();
 }
 
 // 6. Register exception-handling middleware early in request pipeline
