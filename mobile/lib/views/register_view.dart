146887b52da7a465e22f84c5993d3ab59e9c849c
IT24101458-M.G.M. Sanvidu <165678028+ManithSanvidu@users.noreply.github.com>
2026-09-22 23:21:41 +0530

    feat: fixed all the linting issues caught by flutter analyze

--- mobile/lib/views/register_view.dart
+++ mobile/lib/views/register_view.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../core/network/api_client.dart';
 import '../providers/auth_provider.dart';
 import '../core/theme/app_tokens.dart';
 
