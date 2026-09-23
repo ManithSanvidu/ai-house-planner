3c95bc0827214703edaa849cc623800e477d8edb
IT24101458-M.G.M. Sanvidu <165678028+ManithSanvidu@users.noreply.github.com>
2026-09-24 00:07:16 +0530

    feat: resolve Flutter CI analysis warnings

--- mobile/lib/views/home_view.dart
+++ mobile/lib/views/home_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../core/theme/app_tokens.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
